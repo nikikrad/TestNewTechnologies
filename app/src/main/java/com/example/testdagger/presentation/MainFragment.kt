55355f77640f00363ab3abb45adcea608f1c45c3
Александрович Никита <nikikrad228@gmail.com>
2022-07-16 21:36:55 +0300

    test

--- app/src/main/java/com/example/testdagger/presentation/MainFragment.kt
+++ app/src/main/java/com/example/testdagger/presentation/MainFragment.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.testdagger.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
