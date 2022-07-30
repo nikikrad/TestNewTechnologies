b39e5cd5bc98c9613c186842565174ebb7bcf8f0
nikikrad <nikich07192001>
2022-07-30 13:41:57 +0300

    test getValue from Firebase

--- app/src/main/java/com/example/testdagger/presentation/note/NoteFragment.kt
+++ app/src/main/java/com/example/testdagger/presentation/note/NoteFragment.kt
@@ -1,18 +1,26 @@
 package com.example.testdagger.presentation.note
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.testdagger.databinding.FragmentNoteBinding
 import com.example.testdagger.domain.dataclass.TextTranslator
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import retrofit2.http.POST
 
 class NoteFragment: Fragment() {
 
     lateinit var binding: FragmentNoteBinding
+    lateinit var myRef: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +33,35 @@ class NoteFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val database = Firebase.database
-        val myRef = database.getReference("text")
-        myRef.setValue(TextTranslator("Hi", "Привет"))
+        myRef = database.getReference("text")
+        getDataFirebase()
+//        myRef.child("hui").setValue(TextTranslator("buy", "купить"))
+    }
+
+    private fun getDataFirebase(){
+        myRef.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var value = snapshot.value
+//                val mmm = value as TextTranslator
+
+//                Log.e("KEK", mmm.toString())
+
+                Log.e("KEK", value.toString())
+                snapshot.children.forEach {
+                    Log.e("KEK", it.value.toString())
+                }
+                snapshot.children.forEach {
+                    it.children.forEach {
+                        Log.e("KEK", it.value.toString())
+                    }
+                }
+//                Log.e("KEK", snapshot.childrenCount.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Error", error.toString())
+            }
+
+        })
     }
 }
